aa7d586cd5b2eeba38335ff86e8fe071e8fdc004
Brian Muchunku <brauxer@gmail.com>
2022-01-09 03:46:27 +0300

    Formulate the corresponding function 'listQuoteInput' in the root component logic file, to be used to assign the form Quote values entered and submitted by a user via the template's modal form, into a newly generated designated 'objectQuote' Object as the two properties of the objectQuote instance of every user contributor's Quote entry, to be appended to our main app component view's designated table row section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,7 +14,7 @@ export class AppComponent {
 // submitted through the modal hosted form by a user contributor.
 
     quoteInputs = Array();
-    
+
 
   constructor(private ngModal:NgbModal){}
 
@@ -26,5 +26,42 @@ export class AppComponent {
 
     }
 
+    listQuoteInput(submitEvent:Event){
 
-}
+  // The 'preventDefault()' method is to be preceeded by a form variable declaration, which is used to 
+  // designate our 'modal' form variable as the target of our 'submit' event i.e. the item our submitEvent 
+  // is assigned to.
+   
+         
+      let form:any/*FormDataEntryValue*/= submitEvent.target
+   
+         submitEvent.preventDefault()
+   
+   
+       
+        let quoteAuthor:string= form.authorNameInput.value;
+       
+        let newUserQuote:string= form.quoteTextArea.value; // Access the form's textArea input quote value submitted 
+                                                           // by a user contributor and assign the value obtained 
+                                                           // to the 'newUserQuote' variable, formulating a similar 
+                                                           // code statement for the form's author name input.
+       
+  
+   // Formulate a Quote Object to enable us hold each individual modal-form Quote entry 
+   // and its associated vote attributes and functions, in the quoteInputs array as the properties
+   // of an instance of our Quote Object Variable 'objectQuote'.
+   
+   let objectQuote = {
+
+    newUserQuote,
+
+    quoteAuthor,
+
+    daysElapsed:new Date(),
+
+    votes:0
+
+    }   
+
+    }
+  }
